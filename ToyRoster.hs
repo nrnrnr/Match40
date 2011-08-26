5254a56d72794e49807ff19e698a35446e697be5
Andrew Gallant (Cheetah) <Andrew.Gallant@tufts.edu>
2011-08-26 12:46:06 -0400

    replace password field with urlid field

--- ToyRoster.hs
+++ ToyRoster.hs
@@ -10,21 +10,21 @@ roster = [ Student { name = fullName "Noah Daniels"
                    , email = "ndaniels@mac.com"
                    , aboutMe = "Driving instructor"
                    , photo = Nothing
-                   , password = ""
+                   , urlid = "aAaA"
                    , status = Enrolled
                    }
          , Student { name = fullName "Norman Ramsey"
                    , email = "nr@cs.tufts.edu"
                    , aboutMe = "Woodshop instructor"
                    , photo = Nothing
-                   , password = ""
+                   , urlid = "bBbB"
                    , status = Enrolled
                    }
          , Student { name = fullName "Andrew Gallant"
                    , email = "agallant@cs.tufts.edu"
                    , aboutMe = "Grilling instructor"
                    , photo = Nothing
-                   , password = ""
+                   , urlid = "cCcC"
                    , status = Enrolled
                    }
          ]
