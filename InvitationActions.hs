640ab0cdf7d4e2d245a175fedaba0a3cbed92bf4
Norman Ramsey <nr@cs.tufts.edu>
2011-08-26 15:02:24 -0400

    a little documentation for invitation logic

--- InvitationActions.hs
+++ InvitationActions.hs
@@ -3,6 +3,7 @@ module InvitationActions
   )
 where
 
+import Data.List
 
 import qualified CourseMail
 import Invitation
@@ -29,41 +30,68 @@ data PendingAction = PA { old_is :: [Invitation] -- ^ untouched
                         , sends  :: IO ()        -- ^ emails to be sent
                         , errors :: [String]     -- ^ messages on blocked actions
                         }
+   -- this is an intermediate state pending some action
 
 
 type Action = PendingAction -> PendingAction
 
-start_action :: IState -> PendingAction
-start_action is = PA is [] (return ()) []
-
-wrap_pure :: Action -> IState -> IState
-wrap_pure action is = map purestamp (new_is final) ++ old_is final
-    where final = action (start_action is)
-
-wrap_monadic :: Action -> IState -> IO IState
-wrap_monadic action is =
-    do stamped <- mapM timestamp (new_is final)
-       sends final
-       return (stamped ++ old_is final)
-    where final = action (start_action is)
-
-----------------------------------------------------------------
 
 -- | What happens after an action
-data IAresult
-    = Blocked IState Message
-    | Acted Invitation IState (IO ())
-        -- ^ Result is a new invitations, the rest of the invitation state,
-        -- and an IO action to perform if the transaction commits.
-        -- The invitation must be timestamped before being added to the state.
+data IAResult
+    = Blocked IState Message -- ^ The action is disallowed
+    | Acted (IO IState)      -- ^ Launch emails and return new state
              
 type Message = String
 
 
--- | timestamp any outstanding invitation and return invitation state
-finalIState :: IAresult -> IO IState
-finalIState (Blocked s _) = return s
-finalIState (Acted i is _) = timestamp i >>= return . (:is)
+-- Normal actions are converted into a monadic result, but may be pure for testing
+wrap_monadic :: Action -> IState -> IAResult
+wrap_pure    :: Action -> IState -> IState
+
+wrap_pure action is = map purestamp (new_is final) ++ old_is final
+    where final = action (start_action is)
+
+wrap_monadic action is =
+    case final of
+      PA { errors = es@(_ : _) } -> Blocked is (concat $ intersperse "\n" $ es)
+      PA { errors = [] } -> Acted $
+          do stamped <- mapM timestamp (new_is final)
+             sends final
+             return (stamped ++ old_is final)
+    where final = action (start_action is)
+          final :: PendingAction
+
+
+start_action :: IState -> PendingAction
+start_action is = PA is [] (return ()) []
+
+
+
+
+----------------------------------------------------------------
+
+-- Higher-order functions for creating actions
+
+
+-- | Change status of an invitation
+transition :: Status -> Status -> Invitation -> Maybe Invitation
+transition old new i = if hasStatus i old then Just (i { status = new}) else Nothing
+    
+liftChange :: String -- ^ error message
+           -> (Invitation -> Maybe Invitation) -- ^ pure change in invitation
+           -> (Student -> Student -> IO a) -- ^ send email
+           -> Invitation -> Action       -- ^ impure action function
+liftChange msg change send inv state =
+    case splitByTo by to (old_is state) of -- find the invitation
+      (Just i, is) | Just i' <- change i -> -- change it
+            state { old_is = is
+                  , new_is = i' : new_is state
+                  , sends  = sends state >> send by to >> return ()
+                  } -- compute the new state
+      _ -> state { errors = msg : errors state } -- or barf
+    where to = offeredTo inv
+          by = offeredBy inv
+
 
 
 -- | One student proposes partnership to another, offering an invitation
@@ -71,22 +99,9 @@ offer :: (Student -> Student -> Bool) -- ^ Policy function says if students are 
       -> Student -- ^ Student making the offer
       -> Student -- ^ Student to whom the offer is made
       -> IState  -- ^ Current state of all invitations
-      -> IAresult
-offer eligible by to invs
-    | paired by = fail "You already have a partner."
-    | paired to = fail (toname ++ " already has a partner.")
-    | not (eligible by to) = fail ("You are not eligible to work with " ++ toname)
-    | Just i <- offered invs to by = accept i invs
-    | otherwise = Acted this others (CourseMail.offer by to)
-  where toname = readableName to
-        paired = isPaired invs
-        fail msg = Blocked invs msg
-        (this, others) = case splitByTo by to invs of
-                           (Just i, is) -> (i, is)
-                           (Nothing, is) -> (newOffer, is)
-        newOffer = I by to Offered [] 
+      -> IAResult
+offer e by to = wrap_monadic $ offer' e by to
             
--- | One student proposes partnership to another, offering an invitation
 offer' :: (Student -> Student -> Bool) -- ^ Policy function says if students are permitted to work together
       -> Student -- ^ Student making the offer
       -> Student -- ^ Student to whom the offer is made
@@ -108,9 +123,8 @@ offer' eligible by to state@(PA old new sends errors)
             
                  
 -- | Accept an invitation
-accept :: Invitation -> IState -> IAresult
-accept inv invs = Acted (inv { status = Accepted }) invs
-                  (CourseMail.accept (offeredTo inv) (offeredBy inv))
+accept :: Invitation -> IState -> IAResult
+accept inv = wrap_monadic $ accept' inv
 
 accept' :: Invitation -> Action
 accept' inv state@(PA old new sends errors) =
@@ -126,52 +140,25 @@ accept' inv state@(PA old new sends errors) =
           merge (f:fs) = f . merge fs
           offers = filter (flip hasStatus Offered) old
 
-p /\ q = \x -> p x && q x
+          p /\ q = \x -> p x && q x
 
---Acted (inv { status = Accepted }) invs
---                  (CourseMail.accept (offeredTo inv) (offeredBy inv))
 
 -- | Decline an invitation
-decline :: Invitation -> IState -> IAresult
-decline inv invs = Acted (inv { status = Declined }) invs
-                  (CourseMail.decline (offeredTo inv) (offeredBy inv))
+decline :: Invitation -> IState -> IAResult
+decline inv = wrap_monadic $ decline' inv
 
--- | Change status of an invitation
-transition :: Status -> Status -> Invitation -> Maybe Invitation
-transition old new i = if hasStatus i old then Just (i { status = new}) else Nothing
-    
-liftChange :: String -- ^ error message
-           -> (Invitation -> Maybe Invitation) -- ^ pure change in invitation
-           -> (Student -> Student -> IO a) -- ^ send email
-           -> Invitation -> Action       -- ^ impure action function
-liftChange msg change send inv state =
-    case splitByTo by to (old_is state) of
-      (Just i, is) | Just i' <- change i ->
-            state { old_is = is
-                  , new_is = i' : new_is state
-                  , sends  = sends state >> send by to >> return () }
-      _ -> barf msg state
-    where to = offeredTo inv
-          by = offeredBy inv
-
--- | Decline an invitation
 decline' :: Invitation -> Action
 decline' = liftChange "Declined an invitation, but could not find the offer"
            (transition Offered Declined) (flip CourseMail.decline)
 
 -- | Withdraw an invitation
+withdraw :: Invitation -> IState -> IAResult
+withdraw inv = wrap_monadic $ withdraw' inv
 withdraw' :: Invitation -> Action
 withdraw' = liftChange "Withdrew an invitation, but could not find the offer"
             (transition Offered Withdrawn) CourseMail.withdraw
 
-barf :: String -> Action
-barf msg state = state { errors = msg : errors state }
-
-    
--- | Withdraw an invitation
-withdraw :: Invitation -> IState -> IAresult
-withdraw inv invs = Acted (inv { status = Withdrawn }) invs
-                    (CourseMail.withdraw (offeredBy inv) (offeredTo inv))
+----------------------------------------------------------------
 
 
 -- at most one A -> B
