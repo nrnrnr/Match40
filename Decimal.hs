b01776c2c82c1d0271ad30b6d486b3d9ebb94c21
Norman Ramsey <nr@cs.tufts.edu>
2012-05-19 18:41:23 -0400

    passed some tests in word lists

--- Decimal.hs
+++ Decimal.hs
@@ -1,7 +1,7 @@
 module Decimal
-       ( digits, number
+       ( digits, number, ratDigits
        , d10
-       , inInterval
+       , inInterval, minimalLength, inRange
        )
 where
   
@@ -12,9 +12,13 @@ import Test.QuickCheck
 
 digits :: Integral a => a -> Ratio a -> Ratio a -> [a]
 digits base lo hi =
-    if r dplus < r base * hi then
-      if dplus == 0 then [] else [dplus]
-    else d0 : digits base (expand lo) (expand hi)
+    if r 0 <= lo && lo < hi && hi <= r 1 then
+      if r dplus < r base * hi then
+        if dplus == 0 then [] else [dplus]
+      else d0 : digits base (expand lo) (expand hi)
+    else
+      error $ "interval [" ++ show lo ++ ", " ++ show hi ++
+              ") outside the unit interval"
   where dplus = myceil (r base * lo)
         d0    = dplus - 1
         expand x = r base * x - r d0
@@ -26,13 +30,24 @@ number :: Integral a => a -> [a] -> Ratio a
 number base [] = 0
 number base (d:ds) = (fromIntegral d + number base ds) / fromIntegral base
 
+ratDigits base r = digits base r (nextrat r)
 
-inInterval base (NonNegative num) (Positive den) = ok
+nextrat r = (numerator r + 1) % denominator r
+
+inInterval base (NonNegative num) (Positive den') = ok
   where x' = number base (digits base (num % den) (inc num % den))
         x = num % den
         x1 = inc num % den
         ok = x <= x' && x' < x1
         inc n = n + 1
+        den = den' `max` num + 1
+
+inRange (Positive base') (NonNegative num) (Positive den') = ok
+  where ok = all inrange (digits base (num % den) (inc num % den))
+        inrange d = 0 <= d && d < base
+        inc n = n + 1
+        den = den' `max` (num + 1)
+        base = base' + 1
 
 minimalLength base (NonNegative num) (Positive den) = not (null ds) ==> ok
   where x' = number base (init ds)
