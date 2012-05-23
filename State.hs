05222d91378c74a30779fab3e1afe1f9daef50d1
Norman Ramsey <nr@cs.tufts.edu>
2012-05-23 11:12:54 -0400

    user lookup

--- State.hs
+++ State.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE TypeFamilies, DeriveDataTypeable, TemplateHaskell #-}
 module State ( Database(..), User(..)
              , emptyDatabase
+             , findUser, UserFound(..)
              ) 
 where
 
 import Data.Acid
+import Data.Monoid
 import Data.SafeCopy
 import Data.Typeable
 
@@ -21,3 +23,23 @@ $(deriveSafeCopy 0 'base ''Database)
   
 emptyDatabase :: Database
 emptyDatabase = Database [] [] []
+
+data UserFound = UserFound User
+               | UserNotFound
+               | Ambiguous String
+                   
+instance Monoid UserFound where
+  mempty = UserNotFound
+  UserFound a `mappend` _ = UserFound a
+  UserNotFound `mappend` m = m
+  Ambiguous msg `mappend` m = Ambiguous msg
+  
+findUser s users = by "unique ID" (show . uid) `mappend`
+                   select "other ID" (any (==s) . map show . otherIds) `mappend`
+                   by "full name" (show . name . profile) `mappend`
+                   by "last name" (lastName . name . profile)
+  where by what f = select what ((==s) . f)
+        select what p = case filter p users of
+                          [] -> UserNotFound
+                          [u] -> UserFound u
+                          _ -> Ambiguous what
